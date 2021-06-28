5f02352b12775ccc53b506521ebf371818a8ee35
frattezi <pfrattezi@gmail.com>
2021-06-28 11:47:33 -0300

    feat: update main project with new modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,30 @@ terraform {
   required_version = ">= 1.0.1"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-1"
+provider "aws" {}
+
+module "IAM" {
+  source = "./modules/IAM"
 }
 
+module "vpc" {
+  source = "./modules/VPC"
+}
 
 module "ec2_instace" {
-  source = "./modules/ec2"
+  source = "./modules/EC2"
+  
+  default_vpc = module.vpc.default_vpc_id
+  public_subnet_id = module.vpc.subnet_public
+}
+
+module "dynamoDB" {
+  source = "./modules/DynamoDB"
+  
+}
+
+module "lambda_dynamo_user" {
+  source = "./modules/Lambda"
+  
+  lambda_iam_role_arn = module.IAM.lambda_iam_role_arn
 }
