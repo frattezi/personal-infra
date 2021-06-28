36074129d06c3a9e216b9e68479651016afcec11
frattezi <pfrattezi@gmail.com>
2021-06-28 11:46:22 -0300

    feat: create base IAM configuration

---
+++ terraform/modules/IAM/main.tf
@@ -0,0 +1,22 @@
+##############
+# Lambda IAM #
+##############
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "database_handler_role" {
+  name = "database-handler-role"
+
+  assume_role_policy = file("${path.module}/policies/lambda_assume_role.json")
+}
+
+resource "aws_iam_policy" "database_handler_policy" {
+    name        = "database-handler-policy"
+    description = ""
+    policy = file("${path.module}/policies/allow_dynamo_operation.json")
+}
+
+resource "aws_iam_role_policy_attachment" "database_handler_attach" {
+    role       = "${aws_iam_role.database_handler_role.name}"
+    policy_arn = "${aws_iam_policy.database_handler_policy.arn}"
+}
---
+++ terraform/modules/IAM/output.tf
@@ -0,0 +1,3 @@
+output "lambda_iam_role_arn" {
+  value = aws_iam_role.database_handler_role.arn
+}
